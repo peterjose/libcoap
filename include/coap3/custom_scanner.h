35fb5e493006970943c4dfaafe9035a47319e8e3
Peter Jose <peterkjose@gmail.com>
2022-01-16 15:46:13 +0100

    customised for creating lib

---
+++ include/coap3/custom_scanner.h
@@ -0,0 +1,17 @@
+/**
+ * @file custom_scanner.h
+ * @author Peter Jose
+ * @brief 
+ * @version 0.1
+ * @date 2022-01-16
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef _CUSTOM_SCANNER_H_
+#define _CUSTOM_SCANNER_H_
+
+int custom_scanner_fn(int argc, char **argv);
+
+#endif /* _CUSTOM_SCANNER_H_ */
